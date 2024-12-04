75a98807a9fae7801d3039cbee8baea322d4f3ca
Sqwid-member <bubalovlad272@gmail.com>
2024-12-04 21:56:42 +0100

    script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const button = document.getElementById('redirectButton');
 // Додавання події кліку
 button.addEventListener('click', () => {
     // Перенаправлення на інший сайт
-    window.location.href = 'https://pl.xhumster.com'; // Замініть на потрібну URL
+    window.location.href = 'https://google.com'; // Замініть на потрібну URL
 });

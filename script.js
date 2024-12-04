1ab63f43253c6005796873bb9076514df7b09918
Sqwid-member <bubalovlad272@gmail.com>
2024-12-04 22:22:09 +0100

    script.js

--- script.js
+++ script.js
@@ -67,3 +67,55 @@ adminPassword.addEventListener('blur', hideAdminPanel);
 
 incrementViewCount();
 updateCounts();
+const canvas = document.getElementById('rippleCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ripples = [];
+
+class Ripple {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 0;
+        this.alpha = 1;
+    }
+
+    draw() {
+        if (this.alpha <= 0) return;
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.strokeStyle = `rgba(200, 200, 200, ${this.alpha})`;
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        ctx.closePath();
+
+        this.radius += 2;
+        this.alpha -= 0.02;
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ripples.forEach((ripple, index) => {
+        ripple.draw();
+        if (ripple.alpha <= 0) {
+            ripples.splice(index, 1);
+        }
+    });
+
+    requestAnimationFrame(animate);
+}
+
+canvas.addEventListener('click', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    ripples.push(new Ripple(x, y));
+});
+
+animate();
